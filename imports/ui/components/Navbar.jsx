3c44e69b4b5085d25f9f6f69f895a871de53b1f9
JeremyChen <jeremychen0127@gmail.com>
2016-07-08 23:19:33 -0400

    Added navbar

---
+++ imports/ui/components/Navbar.jsx
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { createContainer } from 'meteor/react-meteor-data';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import Drawer from 'material-ui/Drawer';
+import Divider from 'material-ui/Divider';
+import IconButton from 'material-ui/IconButton';
+import FontIcon from 'material-ui/FontIcon';
+import MenuItem from 'material-ui/MenuItem';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const styles = {
+  leftNavMenuItem: {
+    fontFamily: 'Patua One, cursive',
+    color: '#455a64',
+  },
+
+  leftNavMenuItemIcon: {
+    color: '#546e7a',
+  },
+
+  leftNavMenuItemMobile: {
+    fontSize: '25px',
+    fontFamily: 'Patua One, cursive',
+    height: '70px',
+    paddingTop: '10px',
+    color: '#455a64',
+  },
+
+  leftNavMenuItemMobileIcon: {
+    fontSize: '30px',
+    marginTop: '8px',
+    color: '#546e7a',
+  }
+};
+
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      leftNavOpen: false,
+    };
+
+    this.handleLeftNavToggle = (event) => {
+      event.preventDefault();
+      this.setState({leftNavOpen: !this.state.leftNavOpen});
+    }
+
+    this.handleLogout = () => {
+      Meteor.logout(() => {});
+      FlowRouter.go(FlowRouter.path("login"));
+    }
+  }
+
+  getChildContext() {
+    return {muiTheme: getMuiTheme(baseTheme)};
+  }
+
+  renderLeftNav() {
+    return (
+      <div >
+        <Drawer open={this.state.leftNavOpen} docked={false} className="hide-on-small-and-down"
+                onRequestChange={(open, reason) => { this.setState({leftNavOpen:open}) }}
+                width={200} style={{zIndex:9999}}>
+
+          <MenuItem style={styles.leftNavMenuItem} linkButton={true} href="/profile" primaryText="Log Out"
+                    leftIcon={<FontIcon style={styles.leftNavMenuItemIcon} className="material-icons">exit_to_app</FontIcon>}/>
+        </Drawer>
+
+        <Drawer open={this.state.leftNavOpen} docked={false} className="hide-on-med-and-up"
+                onRequestChange={(open, reason) => { this.setState({leftNavOpen:open}) }}
+                width={250} style={{zIndex:9999}}>
+          <MenuItem style={styles.leftNavMenuItemMobile} linkButton={true} href="/profile" primaryText="Log Out"
+                    leftIcon={<FontIcon style={styles.leftNavMenuItemMobileIcon} className="material-icons">exit_to_app</FontIcon>}/>
+        </Drawer>
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="navbar">
+        <ul>
+          <div>
+            <div className="hide-on-med-and-up">
+              <li>
+                <i className="material-icons"
+                   style={{color: 'white', cursor: 'pointer', fontSize: '45px', margin: '20px'}}
+                   onClick={this.handleLeftNavToggle}>
+                  menu
+                </i>
+              </li>
+              <li style={{marginTop: '10px', float: 'right'}}><a id="logo" href="/" style={{fontSize: '25px'}}>Simple 4 Chat</a></li>
+            </div>
+
+            <div className="hide-on-small-and-down">
+              <li style={{marginTop: '5px', marginLeft: '10px'}}>
+                <i className="material-icons"
+                   style={{color: 'white', cursor: 'pointer', fontSize: '30px', margin: '10px'}}
+                   onClick={this.handleLeftNavToggle}>
+                  menu
+                </i>
+              </li>
+              <li style={{float: 'right'}}><a id="logo" href="/" style={{fontSize: '20px'}}>Simple 4 Chat</a></li>
+            </div>
+          </div>
+        </ul>
+        {this.renderLeftNav()}
+      </div>
+    );
+  }
+}
+
+Navbar.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired
+};
--- imports/ui/layouts/Layout.jsx
+++ imports/ui/layouts/Layout.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Navbar from '../components/Navbar'
 
 export const Layout =  ({content}) => (
   <div className="app-root">
+    <Navbar />
+    <br />
     <div className="container">
       {content}
     </div>
