99c424cbfa22f8b53d95ba1d68d4067f1ac75c58
JeremyChen <jeremychen0127@gmail.com>
2016-07-09 00:49:14 -0400

    Added signup page ui

--- imports/ui/components/accounts/Login.jsx
+++ imports/ui/components/accounts/Login.jsx
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
-import AccountsUIWrapper from './AccountsUIWrapper';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styles = {
+  inputField: {
+    fontSize: '16px',
+  },
+  inputDiv: {
+    paddingRight: '30px',
+  }
+};
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSignUp = (username, password) => {
+      Accounts.createUser({username: username, password: password});
+    }
+  }
+
+  getChildContext() {
+    return {muiTheme: getMuiTheme(baseTheme)};
+  }
+
   render() {
     return (
       <div className="row">
-        <div className="col offset-m3 m6">
           <br /><br />
           <br /><br />
-          <div className="center">
-            <AccountsUIWrapper />
-          </div>
-          <br /><br />
-        </div>
+          <form>
+            <div className="row">
+              <div className="col offset-s1 s10 offset-m3 m6 offset-l4 l4 input-field" style={styles.inputDiv}>
+                <input id="username" type="text" placeholder="Your UserName" className="validate" style={styles.inputField}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col offset-s1 s10 offset-m3 m6 offset-l4 l4 input-field" style={styles.inputDiv}>
+                <input id="password" type="password" placeholder="Your Password" className="validate" style={styles.inputField}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col offset-s1 s10 offset-m3 m6 offset-l4 l4 input-field" style={styles.inputDiv}>
+                <input id="password" type="password" placeholder="Re-enter Your Password" className="validate" style={styles.inputField}/>
+              </div>
+            </div>
+            <div className="row">
+              <div className="center">
+                <RaisedButton label="Sign Up" backgroundColor="#ffe0b2"/>
+              </div>
+            </div>
+          </form>
       </div>
     );
   }
 }
+
+Login.childContextTypes = {
+  muiTheme: React.PropTypes.object.isRequired
+};
