c1e54406146f47adc0dab0a86f35144fba14d2ce
JeremyChen <jeremychen0127@gmail.com>
2016-07-09 01:40:53 -0400

    Added signup functionality

--- imports/ui/components/accounts/Signup.jsx
+++ imports/ui/components/accounts/Signup.jsx
@@ -16,8 +16,37 @@ export default class Signup extends Component {
   constructor(props) {
     super(props);
 
-    this.handleSignUp = (username, password) => {
-      Accounts.createUser({username: username, password: password});
+    this.state = {
+      username: '',
+      password: '',
+      passwordAgain: '',
+      isPasswordVerified: true,
+      isUsernameValid: true,
+    };
+
+    this.handleUsernameChange = (event) => {
+      let existingUsername = Meteor.users.findOne({username: event.currentTarget.value});
+      if (existingUsername) {
+        this.setState({isUsernameValid: false});
+      } else {
+        this.setState({isUsernameValid: true});
+      }
+      this.setState({username: event.currentTarget.value});
+    };
+
+    this.handlePasswordChange = (event) => {
+      let isSamePassword = event.currentTarget.value === this.state.passwordAgain;
+      this.setState({password: event.currentTarget.value, isPasswordVerified: isSamePassword});
+    };
+
+    this.handlePasswordAgainChange = (event) => {
+      let isSamePassword = this.state.password === event.currentTarget.value;
+      this.setState({passwordAgain: event.currentTarget.value, isPasswordVerified: isSamePassword});
+    };
+
+    this.handleSignUp = () => {
+      Accounts.createUser({username: this.state.username, password: this.state.password});
+      this.setState({username: '', password: '', passwordAgain: ''});
     }
   }
 
@@ -27,28 +56,34 @@ export default class Signup extends Component {
 
   render() {
     return (
-      <div className="row">
+      <div>
         <br /><br />
         <br /><br />
         <form>
           <div className="row">
             <div className="col offset-s1 s10 offset-m3 m6 offset-l4 l4 input-field" style={styles.inputDiv}>
-              <input id="username" type="text" placeholder="Your UserName" className="validate" style={styles.inputField}/>
+              <input value={this.state.username} onChange={this.handleUsernameChange}
+                     id="username" type="text" placeholder="Your UserName" className="validate" style={styles.inputField}/>
+              <div className={this.state.isUsernameValid ? 'hide' : ''} style={{color: 'red'}}>This username has been taken</div>
             </div>
           </div>
           <div className="row">
             <div className="col offset-s1 s10 offset-m3 m6 offset-l4 l4 input-field" style={styles.inputDiv}>
-              <input id="password" type="password" placeholder="Your Password" className="validate" style={styles.inputField}/>
+              <input value={this.state.password} onChange={this.handlePasswordChange}
+                     id="password" type="password" placeholder="Your Password" className="validate" style={styles.inputField}/>
             </div>
           </div>
           <div className="row">
             <div className="col offset-s1 s10 offset-m3 m6 offset-l4 l4 input-field" style={styles.inputDiv}>
-              <input id="password" type="password" placeholder="Re-enter Your Password" className="validate" style={styles.inputField}/>
+              <input value={this.state.passwordAgain} onChange={this.handlePasswordAgainChange}
+                     id="password" type="password" placeholder="Re-enter Your Password" className="validate" style={styles.inputField}/>
+              <div className={this.state.isPasswordVerified ? 'hide' : ''} style={{color: 'red'}}>Password does not match</div>
             </div>
           </div>
           <div className="row">
             <div className="center">
-              <RaisedButton label="Sign Up" backgroundColor="#ffe0b2"/>
+              <RaisedButton onClick={this.handleSignUp} label="Sign Up" backgroundColor="#ffe0b2"
+                            disabled={!this.state.isPasswordVerified || !this.state.isUsernameValid || !this.state.username} />
             </div>
           </div>
         </form>
