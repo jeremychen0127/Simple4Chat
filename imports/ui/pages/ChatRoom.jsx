95f2df8972a5d79738a00b17167cf379e38c235f
JeremyChen <jeremychen0127@gmail.com>
2016-07-20 23:24:49 -0400

    Prototyped the message input

--- imports/ui/pages/ChatRoom.jsx
+++ imports/ui/pages/ChatRoom.jsx
@@ -1,18 +1,8 @@
 import React, { Component } from 'react';
-import FlatButton from 'material-ui/FlatButton';
 import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 export default class ChatRoom extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleLogout = () => {
-      Meteor.logout(() => {});
-      FlowRouter.go(FlowRouter.path("login"));
-    }
-  }
-
   getChildContext() {
     return {muiTheme: getMuiTheme(baseTheme)};
   }
@@ -21,12 +11,7 @@ export default class ChatRoom extends Component {
     return (
       <div className="row">
         <div className="col offset-m3 m6">
-          <br /><br />
-          <br /><br />
-          <div className="center" >
-            <FlatButton label="Sign Out" onClick={this.handleLogout}/>
-          </div>
-          <br /><br />
+          <textarea placeholder="Type your message..."/>
         </div>
       </div>
     );
